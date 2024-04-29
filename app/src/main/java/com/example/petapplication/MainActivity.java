6c87c9cba3adc68afe59058affca5cacae0d4c61
erikas9987 <144891507+erikas9987@users.noreply.github.com>
2024-04-29 15:52:10 +0300

    Created a login activity and set a button to redirect to it PET-8

--- app/src/main/java/com/example/petapplication/MainActivity.java
+++ app/src/main/java/com/example/petapplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.petapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,11 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
     }
+
+    public void goToLogin(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+
 }
