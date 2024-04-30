acac954c1b7a3c24ad59a36a00cfc357554c9a60
erikas9987 <144891507+erikas9987@users.noreply.github.com>
2024-04-30 08:52:20 +0300

    Added token retrieval process for login #PET-8

--- app/src/main/java/com/example/petapplication/LoginActivity.java
+++ app/src/main/java/com/example/petapplication/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.petapplication;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.petapplication.databinding.ActivityLoginBinding;
+import com.example.petapplication.helpers.Constants;
+import com.example.petapplication.model.Login;
 import com.example.petapplication.validators.BaseValidator;
 import com.example.petapplication.validators.EmailValidator;
 import com.example.petapplication.validators.EmptyValidator;
@@ -14,18 +22,24 @@ import com.example.petapplication.validators.PasswordValidator;
 import com.example.petapplication.validators.ValidationResult;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        sharedPreferences = this.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
         setContentView(binding.getRoot());
     }
 
     public void retrieveToken(View view) {
+
         Editable emailField = binding.loginEmailAddress.getText();
         if (emailField == null) {
             binding.loginEmailAddress.setError(getString(R.string.text_validation_error_blank_text));
@@ -55,7 +69,46 @@ public class LoginActivity extends AppCompatActivity {
         if (!(emailResult.isSuccess() && passwordResult.isSuccess()))
             return;
 
+        Login login = new Login();
+        login.setEmail(emailText);
+        login.setPassword(passwordText);
 
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = Constants.LOGIN_URL;
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("email", emailText);
+            jsonObject.put("password", passwordText);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                jsonObject,
+                response -> {
+                    try {
+                        saveToken(response.getString("token"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Snackbar.make(
+                        view,
+                        "Failed to log in!",
+                        Snackbar.LENGTH_LONG
+                ).show()
+        );
+
+        queue.add(request);
+    }
+
+    private void saveToken(String token) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("token", token);
+        editor.apply();
     }
 
 
---
+++ app/src/main/java/com/example/petapplication/helpers/Constants.java
@@ -0,0 +1,11 @@
+package com.example.petapplication.helpers;
+
+public class Constants {
+
+    public static final String IP_ADDRESS = "192.168.0.7";
+    public static final String PORT = "8080";
+    public static final String BASE_URL = "http://" + IP_ADDRESS + ":" + PORT;
+    public static final String AUTH_URL = BASE_URL + "/auth";
+    public static final String LOGIN_URL = AUTH_URL + "/authenticate";
+
+}
